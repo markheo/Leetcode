59aac0a5080a9d75af7fba378207b1fa83d6f536
Mark Heo <83532458+markheo@users.noreply.github.com>
2024-01-12 10:35:34 +0900

    Time: 38 ms (54.49%), Space: 17.4 MB (14.16%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,26 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        def dfs(index):
-            result = ""
-            num = 0
+        stack = []
+        num = 0
+        curString = ''
 
-            while index < len(s):
-                char = s[index]
-                if char.isdigit():
-                    num = num * 10 + int(char)
-                elif char == '[':
-                    # 재귀 호출로 대괄호 안의 문자열 처리
-                    index, decoded_string = dfs(index + 1)
-                    result += num * decoded_string
-                    num = 0
-                elif char == ']':
-                    # 현재 대괄호의 문자열 반환
-                    return index, result
-                else:
-                    result += char
-                index += 1
+        for char in s:
+            if char.isdigit():
+                num = num * 10 + int(char)
+            elif char == '[':
+                # 숫자와 현재 문자열을 스택에 푸시
+                stack.append((num, curString))
+                num, curString = 0, ''
+            elif char == ']':
+                # 스택에서 숫자와 이전 문자열을 팝
+                prevNum, prevString = stack.pop()
+                curString = prevString + prevNum * curString
+            else:
+                curString += char
 
-            return result
-
-        return dfs(0)
+        return curString
         
